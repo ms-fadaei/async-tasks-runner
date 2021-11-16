9d50b7c5c8fc89077d974b0e161f965a5f2736fa
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2021-11-16 09:04:06 +0330

    refactor: rewrite helpers function
    Providing a way to cancel provided timeouts with best practices

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,17 +2,48 @@ import { Task, ParallelTask, SerialTask, PipelineTask } from './runners/types'
 
 export const timeoutResult: Symbol = Symbol('timeout')
 
-export function createTimeoutResolve<T> (timeout: number, response: T): Promise<T> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout, response)
+export function createTimeoutResolve<T> (timeout: number, response: T, cancelToken?: Promise<unknown>): Promise<T> {
+  // Returning the promise Immediately when cancelToken is not provided
+  if (typeof cancelToken === 'undefined') {
+    return new Promise((resolve) => {
+      setTimeout(resolve, timeout, response)
+    })
+  }
+
+  // clearing timeout with cancel token
+  let timeoutId: ReturnType<typeof setTimeout>
+  const _clearTimeout = () => clearTimeout(timeoutId)
+  cancelToken.then(_clearTimeout, _clearTimeout)
+
+  const timeoutPromise = new Promise((resolve) => {
+    timeoutId = setTimeout(resolve, timeout, response)
   })
+
+  // Clearing the timeout if the cancel-token promise settled first.
+  return Promise.race([timeoutPromise, cancelToken]) as Promise<T>
 }
 
-export function createTimeoutReject<T> (timeout: number, error: T): Promise<T> {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  return new Promise((resolve, reject) => {
-    setTimeout(reject, timeout, error)
+export function createTimeoutReject<T> (timeout: number, error: T, cancelToken?: Promise<unknown>): Promise<T> {
+  // Returning the promise Immediately when cancelToken is not provided
+  if (typeof cancelToken === 'undefined') {
+    // eslint-disable-next-line promise/param-names
+    return new Promise((_resolve, reject) => {
+      setTimeout(reject, timeout, error)
+    })
+  }
+
+  // clearing timeout with cancel token
+  let timeoutId: ReturnType<typeof setTimeout>
+  const _clearTimeout = () => clearTimeout(timeoutId)
+  cancelToken.then(_clearTimeout, _clearTimeout)
+
+  // eslint-disable-next-line promise/param-names
+  const timeoutPromise = new Promise((_resolve, reject) => {
+    timeoutId = setTimeout(reject, timeout, error)
   })
+
+  // Clearing the timeout if the cancel-token promise settled first.
+  return Promise.race([timeoutPromise, cancelToken]) as Promise<T>
 }
 
 /* eslint-disable no-redeclare */
@@ -20,6 +51,13 @@ export function createTaskWithTimeout<T> (task: ParallelTask<T>, timeout: number
 export function createTaskWithTimeout<T> (task: SerialTask<T>, timeout: number): SerialTask<T>;
 export function createTaskWithTimeout<T> (task: PipelineTask<T>, timeout: number): PipelineTask<T>;
 export function createTaskWithTimeout<T> (task: Task<T>, timeout: number): Task<T> {
-  return (arg: T) => Promise.race([task(arg), createTimeoutReject(timeout, timeoutResult)]) as Promise<T>
+  return function taskWithTimeout (arg: T) {
+    const taskPromise = task(arg)
+
+    // Use taskPromise as timeoutRejects cancelToken
+    const timeoutPromise = createTimeoutReject(timeout, timeoutResult, taskPromise)
+
+    return Promise.race([taskPromise, timeoutPromise]) as Promise<T>
+  }
 }
 /* eslint-enable no-redeclare */
